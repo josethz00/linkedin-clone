47e6243be4d658f05a087dc9b9afd19e64f10a48
josethz00 <jtsoares17@hotmail.com>
2020-08-27 12:10:18 -0300

    Final commit

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { Container } from './styles';
+
+import MobileHeader from '../MobileHeader';
+import DesktopHeader from '../DesktopHeader';
+import LeftColumn from '../LeftColumn';
+import MiddleColumn from '../MiddleColumn';
+import RightColumn from '../RightColumn';
+import AdBanner from '../AdBanner';
+
+const Layout: React.FC = ()=>{
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            setIsLoading(false);
+        }, 1000);
+    }, []);
+
+    return(
+        <Container>
+            <MobileHeader />
+            <DesktopHeader />
+            <span>
+                {!isLoading ? (<AdBanner />) : null}
+            </span>
+            <main>
+                <LeftColumn isLoading={isLoading} />
+                <MiddleColumn isLoading={isLoading} />
+                <RightColumn isLoading={isLoading} />
+            </main>
+        </Container>
+    );
+
+};
+
+export default Layout;
